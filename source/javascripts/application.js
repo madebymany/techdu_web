afabd278e5ab765fe32be2c454adbb7ff49edc8b
Oli Matthews <olvado@gmail.com>
2015-03-04 12:38:57 +0000

    setup bb js

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -8,10 +8,26 @@ var initializer = new BigBird.Initializer({
   modules: {
     common: {
       initialize: function initializeAction() {
+        // Any JS will fire on every page (after DOM loaded)
         console.log(BigBird.VERSION);
         console.log($.fn.jquery);
       }
+    },
+    root: {
+      index: function() {
+        // Only fire on index.html.erb
+        console.log('index');
+      },
+      events: function() {
+        // Only fire on events.html.erb
+        console.log('events');
+      },
+      contact: function() {
+        // Only fire on contact.html.erb
+        console.log('contact');
+      }
     }
+
   }
 });
 
